cb4ea4ed34c51ae25f7262d93cd682c9628cc3dc
Frank Rowe <fsrowe@gmail.com>
2015-01-12 10:56:38 -0500

    remove sector colors

--- public/js/modules/FilterLabelView.js
+++ public/js/modules/FilterLabelView.js
@@ -18,7 +18,10 @@ var FilterLabelView = Backbone.View.extend({
   },
   style: function() {
     if (this.model.get('color')) {
-      this.$el.find('button').css('background-color', this.model.get('color'))
+      if ((this.model.get('type') === 'sector' && Dashboard.activetab === 'efficiency')
+         || (Dashboard.activetab !== 'efficiency' && this.model.get('type') !== 'sector')) {
+        this.$el.find('button').css('background-color', this.model.get('color'))
+      }
     }
   },
   activate: function(filter, e){
