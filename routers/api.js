748425e30a4156719f200a2266b1ad67e979818c
Frank Rowe <fsrowe@gmail.com>
2015-01-20 15:44:14 -0500

    only show non regional projects on map

--- routers/api.js
+++ routers/api.js
@@ -474,6 +474,7 @@ api.get('/getProjectsByPoint', function(req, res){
     delete req.query.tech_field
     var conditions = filter.conditions(req.query)
     conditions[tech_field] = tech
+    conditions.regional = false
     mongo.db.collection(req.query.tab).find(conditions).toArray(function(err, data) {
       returnData(req, res, data)
     })
